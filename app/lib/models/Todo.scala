78bc083b7485e2b8793dc7d7d9a520477bdcc55e
taretmch <tomochuri25@gmail.com>
2020-05-27 00:05:43 +0900

    Add Post model

---
+++ app/lib/models/Todo.scala
@@ -0,0 +1,33 @@
+package lib.model
+
+import ixias.model._
+import ixias.util.EnumStatus
+import java.time.LocalDateTime
+
+// Post モデルの定義
+import Post._
+case class Post(
+  id:    Option[Id],
+  title: String,
+  body:  Option[String],
+  updatedAt: LocalDateTime = NOW,
+  createdAt: LocalDateTime = NOW
+) extends EntityModel[Id]
+
+// コンパニオンオブジェクト
+object Post {
+  
+  val  Id         = the[Identity[Id]]
+  type Id         = Long @@ Post
+  type WithNoId   = Entity.WithNoId[Id, Post]
+  type EmbeddedId = Entity.EmbeddedId[Id, Post]
+
+  def apply(title: String, body: String): WithNoId = {
+    new Entity.WithNoId(
+      new Post(
+        title = title,
+        body  = body
+      )
+    )
+  }
+}
