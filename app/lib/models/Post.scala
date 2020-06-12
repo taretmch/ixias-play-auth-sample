ecb6543c41372bdd27be0c25e502be3563708c9b
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2020-06-12 18:15:47 +0900

    Remove type definition of WithNoId and EmbeddedId

--- app/lib/models/Post.scala
+++ app/lib/models/Post.scala
@@ -7,30 +7,25 @@ import java.time.LocalDateTime
 // Post モデルの定義
 import Post._
 case class Post(
-  id:        Option[Id],
-  title:     String,
-  body:      String,
-  public:    Boolean,
+  id:        Option[Id], // 記事 Id
+  title:     String,     // 記事のタイトル
+  body:      String,     // 記事の本文
+  public:    Boolean,    // 公開しているか
   updatedAt: LocalDateTime = NOW,
   createdAt: LocalDateTime = NOW
 ) extends EntityModel[Id]
 
 // コンパニオンオブジェクト
 object Post {
-  
   val  Id         = the[Identity[Id]]
   type Id         = Long @@ Post
-  type WithNoId   = Entity.WithNoId[Id, Post]
-  type EmbeddedId = Entity.EmbeddedId[Id, Post]
 
-  def apply(title: String, body: String, public: Boolean): WithNoId = {
-    new Entity.WithNoId(
-      new Post(
-        id     = None,
-        title  = title,
-        body   = body,
-        public = public
-      )
-    )
+  def apply(title: String, body: String, public: Boolean): Post#WithNoId = {
+    new Post(
+      id     = None,
+      title  = title,
+      body   = body,
+      public = public
+    ).toWithNoId
   }
 }
